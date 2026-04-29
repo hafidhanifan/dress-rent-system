49d7f4bab5c255e92385e94972bcbc3015c6398e
hafidhanifan <hafid.hanifan123@gmail.com>
2026-04-29 19:39:15 +0700

    auth setup backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { AppService } from './app.service';
       database: process.env.DATABASE_NAME,
       entities: [], //nanti diisikan dengan model/tabel
       synchronize: true, //otomatis membuat tabel (hanya untuk development)
-    }),
+    }), AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
