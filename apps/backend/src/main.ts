eed03fcb9e3d43983092d7e0f6060021b8eb8262
hafidhanifan <hafid.hanifan123@gmail.com>
2026-04-30 05:46:36 +0700

    modified:   apps/backend/src/main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); //izinkan Next.js untuk mengakses backend
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // untuk menghapus field yang tidak ada di DTO
+      forbidNonWhitelisted: true, // error jika field tidak dikenal
+      transform: true, // otomatis konversi tipe data
+    }),
+  );
+
+  //izinkan Next.js untuk mengakses backend atau izinkan request dari front end
+  app.enableCors({ origin: 'http://localhost:3000' });
+
   await app.listen(process.env.PORT ?? 3001); //ganti port supaya tidak bentrok
-  console.log('Backend jalan di port 3001');
+  // console.log('Backend jalan di port 3001');
 }
 bootstrap();
