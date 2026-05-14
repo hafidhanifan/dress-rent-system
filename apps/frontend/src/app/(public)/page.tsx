726c92b4bc17288d24e6b1d2a3093f64bfd6a287
hafidhanifan <hafid.hanifan123@gmail.com>
2026-05-14 17:18:53 +0700

    modified:   apps/frontend/src/app/(public)/page.tsx 	modified:   apps/frontend/src/app/layout.tsx

--- apps/frontend/src/app/(public)/page.tsx
+++ apps/frontend/src/app/(public)/page.tsx
@@ -1,21 +1,17 @@
-import Navbar from "@/components/public/Navbar";
 import HeroSection from "@/components/public/Herosection";
 import CategoriesSection from "@/components/public/Categoriessection";
 import QuoteSection from "@/components/public/Quotesection";
 import SpotlightSection from "@/components/public/Spotlightsection";
 import WhyChooseSection from "@/components/public/Whychoose";
-import Footer from "@/components/public/Footer";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <HeroSection />
       <QuoteSection />
       <CategoriesSection />
       <SpotlightSection />
       <WhyChooseSection />
-      <Footer />
     </main>
   );
 }
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -44,5 +44,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// testing trestin testingh
